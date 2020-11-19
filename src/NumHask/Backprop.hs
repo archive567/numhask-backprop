f3ed232adaa75fe65e8c1974d11c3791df64c218
Tony Day <tonyday567@gmail.com>
2020-11-20 09:39:20 +1000

    prior to ghc-8.10.2

--- src/NumHask/Backprop.hs
+++ src/NumHask/Backprop.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -11,87 +10,108 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
-module NumHask.Backprop where
+-- | Backprop instances for NumHask classes.
+module NumHask.Backprop ( ) where
 
-import NumHask.KTuple
+import NumHask.KTuple (KTuple, KTupleF (..), splitKT)
 import NumHask.Prelude as NH
 import Numeric.Backprop as BP
--- import Control.Lens hiding (Magma, magma)
 
 -- | unwrapped BVar orphan instances for numhask
--- Fixme: not sure how to proceed with Normed, Metric
 
--- numhask classes
-instance (Backprop a, Magma a, Reifies s W) =>
-         Magma (BVar s a) where
+-- Magma and Idempotent needed for KleeneAlgebra
+instance
+  (Backprop a, Reifies s W, Magma a) =>
+  Magma (BVar s a)
+  where
   magma = liftOp2 . op2 $ \x y -> (x `magma` y, \g -> (g, g))
 
-instance (Backprop a, Idempotent a, Reifies s W) =>
-         Idempotent (BVar s a)
+instance
+  (Backprop a, Reifies s W, Idempotent a) =>
+  Idempotent (BVar s a)
 
-instance (Backprop a, Additive a, Reifies s W) => Additive (BVar s a) where
+-- numhask classes
+instance (Backprop a, Reifies s W, Additive a) => Additive (BVar s a) where
   (+) = liftOp2 . op2 $ \x y -> (x + y, \g -> (g, g))
   zero = constVar NH.zero
 
-instance (Backprop a, Subtractive a, Reifies s W) =>
-         Subtractive (BVar s a) where
+instance
+  (Backprop a, Reifies s W, Subtractive a) =>
+  Subtractive (BVar s a)
+  where
   negate = liftOp1 . op1 $ \x -> (negate x, negate)
 
-instance (Backprop a, Multiplicative a, Reifies s W) =>
-         Multiplicative (BVar s a) where
+instance
+  (Backprop a, Reifies s W, Multiplicative a) =>
+  Multiplicative (BVar s a)
+  where
   (*) = liftOp2 . op2 $ \x y -> (x * y, \g -> (y * g, x * g))
   one = constVar NH.one
 
-instance (Backprop a, Reifies s W, Subtractive a, Divisive a) =>
-         Divisive (BVar s a) where
+instance
+  (Backprop a, Reifies s W, Divisive a, Subtractive a) =>
+  Divisive (BVar s a)
+  where
   recip = liftOp1 . op1 $ \x -> (recip x, (/ (x * x)) . negate)
 
-instance (Backprop a, Distributive a, Reifies s W) =>
-         Distributive (BVar s a)
+instance
+  (Backprop a, Reifies s W, Distributive a) =>
+  Distributive (BVar s a)
 
-instance (Backprop a, IntegralDomain a, Reifies s W) =>
-         IntegralDomain (BVar s a)
+instance
+  (Backprop a, Reifies s W, IntegralDomain a) =>
+  IntegralDomain (BVar s a)
 
-instance (Backprop a, Reifies s W, InvolutiveRing a) =>
-         InvolutiveRing (BVar s a) where
+instance
+  (Backprop a, Reifies s W, InvolutiveRing a) =>
+  InvolutiveRing (BVar s a)
+  where
   adj = liftOp1 . op1 $ \x -> (adj x, adj)
 
-instance (Backprop a, Reifies s W, StarSemiring a) =>
-         StarSemiring (BVar s a) where
+instance
+  (Backprop a, Reifies s W, StarSemiring a) =>
+  StarSemiring (BVar s a)
+  where
   plus = liftOp1 . op1 $ \x -> (star x, plus)
   star = liftOp1 . op1 $ \x -> (plus x, (* star x))
 
-instance (Backprop a, KleeneAlgebra a, Reifies s W) =>
-         KleeneAlgebra (BVar s a)
+instance
+  (Backprop a, Reifies s W, KleeneAlgebra a) =>
+  KleeneAlgebra (BVar s a)
 
-instance (Backprop a, Field a, Subtractive a, Reifies s W) => Field (BVar s a)
+instance (Backprop a, Reifies s W, Field a) => Field (BVar s a)
 
-instance ( Backprop a
-         , Reifies s W
-         , Additive a
-         , Subtractive a
-         , ExpField a
-         ) =>
-         ExpField (BVar s a) where
+instance
+  ( Backprop a,
+    Reifies s W,
+    ExpField a
+  ) =>
+  ExpField (BVar s a)
+  where
   exp = liftOp1 . op1 $ \x -> (exp x, (exp x *))
   log = liftOp1 . op1 $ \x -> (log x, (/ x))
 
-instance ( Backprop a
-         , Reifies s W -- fixme: why was this needed here?
-         , ExpField a
-         , Subtractive a
-         , TrigField a
-         ) =>
-         TrigField (BVar s a) where
+instance
+  ( Backprop a,
+    Reifies s W,
+    ExpField a,
+    Subtractive a,
+    TrigField a
+  ) =>
+  TrigField (BVar s a)
+  where
   pi = constVar NH.pi
   sin = liftOp1 . op1 $ \x -> (sin x, (* cos x))
   cos = liftOp1 . op1 $ \x -> (cos x, (* negate (sin x)))
   asin = liftOp1 . op1 $ \x -> (asin x, (/ sqrt (NH.one - x * x)))
   acos = liftOp1 . op1 $ \x -> (acos x, (/ sqrt (NH.one - x * x)) . negate)
   atan = liftOp1 . op1 $ \x -> (atan x, (/ (x * x + NH.one)))
+
+  -- atan2 = liftOp2 . op2 $ \x y -> undefined -- (atan x, (/ (x * x + NH.one)))
   sinh = liftOp1 . op1 $ \x -> (sinh x, (* cosh x))
   cosh = liftOp1 . op1 $ \x -> (cosh x, (* sinh x))
   asinh = liftOp1 . op1 $ \x -> (tanh x, (/ cosh x ** (NH.one + NH.one)))
@@ -99,131 +119,161 @@ instance ( Backprop a
   atanh = liftOp1 . op1 $ \x -> (atanh x, (/ (NH.one - x * x)))
 
 divModOp ::
-     (Subtractive a, Integral a) => Op '[ a, a] (KTuple a a)
+  (Subtractive a, Integral a) => Op '[a, a] (KTuple a a)
 divModOp =
   op2 $ \x y ->
-    (let (d, m) = divMod x y
-     in (KT d m, \(KT d' m') -> (d', m' - m' * d)))
+    ( let (d, m) = divMod x y
+       in (KT d m, \(KT d' m') -> (d', m' - m' * d))
+    )
 
 quotRemOp ::
-     (Subtractive a, Integral a) => Op '[ a, a] (KTuple a a)
+  (Subtractive a, Integral a) => Op '[a, a] (KTuple a a)
 quotRemOp =
   op2 $ \x y ->
-    (let (q, r) = quotRem x y
-     in (KT q r, \(KT q' r') -> (q', r' - r' * q)))
+    ( let (q, r) = quotRem x y
+       in (KT q r, \(KT q' r') -> (q', r' - r' * q))
+    )
 
-instance ( Backprop a
-         , Integral a
-         , UpperBoundedField a
-         , FromInteger a
-         , Subtractive a
-         , Reifies s W
-         ) =>
-         Integral (BVar s a) where
+instance
+  ( Backprop a,
+    Integral a,
+    UpperBoundedField a,
+    Subtractive a,
+    Reifies s W
+  ) =>
+  Integral (BVar s a)
+  where
   divMod a b =
     let (splitBV -> KT a' b') = liftOp2 divModOp a b
-    in (a', b')
+     in (a', b')
   quotRem a b =
     let (splitBV -> KT a' b') = liftOp2 quotRemOp a b
-    in (a', b')
+     in (a', b')
 
 properFractionOp ::
-     (Eq a, UpperBoundedField a, ToInteger b, FromInteger a, QuotientField a b)
-  => Op '[ a] (KTuple b a)
+  (Eq a, UpperBoundedField a, QuotientField a b, FromIntegral a b) =>
+  Op '[a] (KTuple b a)
 properFractionOp =
   op1 $ \x ->
     (,)
-      (let (i, r) = properFraction x
-       in KT i r)
-      (\(KT i r) ->
-         if r == NH.zero
-           then nan
-           else NH.fromIntegral i)
+      ( let (i, r) = properFraction x
+         in KT i r
+      )
+      ( \(KT i r) ->
+          if r == NH.zero
+            then nan
+            else NH.fromIntegral i
+      )
 
-instance ( Ord a
-         , Ord b
-         , Backprop a
-         , Backprop b
-         , Field a
-         , Field b
-         , UpperBoundedField b
-         , UpperBoundedField a
-         , FromInteger a
-         , FromIntegral a Integer
-         , Integral b
-         , ToInteger b
-         , FromInteger b
-         , FromIntegral b Integer
-         , Subtractive b
-         , Reifies s W
-         , QuotientField a b
-         ) =>
-         QuotientField (BVar s a) (BVar s b) where
+instance
+  ( Ord a,
+    Ord b,
+    Backprop a,
+    Backprop b,
+    Field a,
+    Field b,
+    UpperBoundedField b,
+    UpperBoundedField a,
+    Integral b,
+    FromIntegral a b,
+    Subtractive b,
+    Reifies s W,
+    QuotientField a b
+  ) =>
+  QuotientField (BVar s a) (BVar s b)
+  where
   properFraction = splitKT . liftOp1 properFractionOp
 
-instance (Backprop a, Reifies s W, Additive a, Signed a) =>
-         Signed (BVar s a) where
-  sign = liftOp1 . op1 $ \x -> (sign x, const NH.zero)
-  abs = liftOp1 . op1 $ \x -> (abs x, (* sign x))
-
-instance (Additive a, Multiplicative a, Backprop a, Reifies s W, MeetSemiLattice a) =>
-  MeetSemiLattice (BVar s a) where
+instance
+  (Additive a, Multiplicative a, Backprop a, Reifies s W, MeetSemiLattice a) =>
+  MeetSemiLattice (BVar s a)
+  where
   (/\) = liftOp2 . op2 $ \x y ->
-    (x /\ y,
-      \g -> ( g * bool NH.zero NH.one (x /\ y == x)
-           , g * bool NH.zero NH.one (x /\ y == x)))
+    ( x /\ y,
+      \g ->
+        ( g * bool NH.zero NH.one (x /\ y == x),
+          g * bool NH.zero NH.one (x /\ y == x)
+        )
+    )
 
-instance (Additive a, Multiplicative a, Backprop a, Reifies s W, JoinSemiLattice a) =>
-  JoinSemiLattice (BVar s a) where
+instance
+  (Additive a, Multiplicative a, Backprop a, Reifies s W, JoinSemiLattice a) =>
+  JoinSemiLattice (BVar s a)
+  where
   (\/) = liftOp2 . op2 $ \x y ->
-    (x \/ y,
-      \g -> ( g * bool NH.zero NH.one (x \/ y == x)
-           , g * bool NH.zero NH.one (x \/ y == x)))
+    ( x \/ y,
+      \g ->
+        ( g * bool NH.zero NH.one (x \/ y == x),
+          g * bool NH.zero NH.one (x \/ y == x)
+        )
+    )
 
-instance (Additive a, Multiplicative a, Backprop a, Reifies s W, Epsilon a) =>
-         Epsilon (BVar s a) where
-  epsilon = constVar epsilon
+instance
+  (Backprop a, Reifies s W, BoundedJoinSemiLattice a, Additive a, Multiplicative a) =>
+  BoundedJoinSemiLattice (BVar s a)
+  where
+  bottom = constVar bottom
 
-instance (Backprop a, Reifies s W, UpperBoundedField a, Subtractive a) =>
-         UpperBoundedField (BVar s a) where
+instance
+  (Backprop a, Reifies s W, BoundedMeetSemiLattice a, Additive a, Multiplicative a) =>
+  BoundedMeetSemiLattice (BVar s a)
+  where
+  top = constVar top
 
-instance (Backprop a, Reifies s W, LowerBoundedField a) =>
-         LowerBoundedField (BVar s a)
+instance
+  (Backprop a, Reifies s W, UpperBoundedField a, Subtractive a) =>
+  UpperBoundedField (BVar s a)
+
+instance
+  (Backprop a, Reifies s W, LowerBoundedField a) =>
+  LowerBoundedField (BVar s a)
 
 instance (Additive b, Backprop b, Reifies s W, FromIntegral a b) => FromIntegral (BVar s a) (BVar s b) where
-  fromIntegral_ =
-    liftOp1 . op1 $ \x -> (,) (NH.fromIntegral_ x) (const NH.zero)
+  fromIntegral =
+    liftOp1 . op1 $ \x -> (,) (NH.fromIntegral x) (const NH.zero)
 
 instance (Additive a, Backprop a, Reifies s W, ToIntegral a b) => ToIntegral (BVar s a) (BVar s b) where
   toIntegral =
     liftOp1 . op1 $ \x -> (,) (NH.toIntegral x) (const NH.zero)
 
 toRatioOp ::
-     (Additive a, ToRatio a b)
-  => Op '[ a] (KTuple b b)
+  (Additive a, ToRatio a b) =>
+  Op '[a] (KTuple b b)
 toRatioOp =
   op1 $ \x ->
     (,)
-      (let (i :% r) = toRatio x
-       in KT i r)
+      ( let (i :% r) = toRatio x
+         in KT i r
+      )
       (\(KT _ _) -> NH.zero)
 
 instance (Additive a, Backprop a, Backprop b, Reifies s W, ToRatio a b) => ToRatio (BVar s a) (BVar s b) where
-  toRatio x = (\(n,d) -> n :% d) $ splitKT (liftOp1 toRatioOp x)
+  toRatio x = (\(n, d) -> n :% d) $ splitKT (liftOp1 toRatioOp x)
 
 fromRatioOp ::
-    (Additive b, Multiplicative b, FromRatio a b)
-  => Op '[b, b] a
+  (Additive b, Multiplicative b, FromRatio a b) =>
+  Op '[b, b] a
 fromRatioOp =
   op2 $ \n d ->
     (,)
-      (fromRatio (n:%d))
+      (fromRatio (n :% d))
       (\_ -> (NH.zero, NH.one))
 
 instance (Additive b, Multiplicative b, Backprop b, Reifies s W, FromRatio a b) => FromRatio (BVar s a) (BVar s b) where
-  fromRatio (n:%d) = liftOp2 fromRatioOp n d 
+  fromRatio (n :% d) = liftOp2 fromRatioOp n d
 
-instance (a ~ b, Signed b, Divisive b, Backprop a, Reifies s W, Normed a b) => Normed (BVar s a) (BVar s b) where
+instance (a ~ b, Signed b, Divisive b, Backprop a, Reifies s W, Norm a b) => Norm (BVar s a) (BVar s b) where
   norm = liftOp1 . op1 $ \x -> (norm x, (* ((norm x) / x)))
 
+instance
+  (Backprop a, Reifies s W, Additive a, Signed a) =>
+  Signed (BVar s a)
+  where
+  sign = liftOp1 . op1 $ \x -> (sign x, const NH.zero)
+  abs = liftOp1 . op1 $ \x -> (abs x, (* sign x))
 
+instance
+  (Additive a, Multiplicative a, Backprop a, Reifies s W, Epsilon a) =>
+  Epsilon (BVar s a)
+  where
+  epsilon = constVar epsilon
--- src/NumHask/KTuple.hs
+++ src/NumHask/KTuple.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE PatternSynonyms #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RebindableSyntax #-}
@@ -11,14 +10,16 @@
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# OPTIONS_GHC -Wall #-}
 
 -- | Higher-Kinded Tuple Trick numbers
 module NumHask.KTuple
-  ( KTupleF(..)
-  , KTuple
-  , splitKT
-  ) where
+  ( KTupleF (..),
+    KTuple,
+    splitKT,
+  )
+where
 
 import NumHask.Prelude
 import Numeric.Backprop hiding (one, zero)
@@ -27,10 +28,11 @@ type family HKD f a where
   HKD Identity a = a
   HKD f a = f a
 
-data KTupleF a b f = KT
-  { ktA :: HKD f a
-  , ktB :: HKD f b
-  }
+data KTupleF a b f
+  = KT
+      { ktA :: HKD f a,
+        ktB :: HKD f b
+      }
   deriving (Generic)
 
 type KTuple a b = KTupleF a b Identity
@@ -41,111 +43,7 @@ instance (Eq a, Eq b) => Eq (KTuple a b) where
 instance (Backprop a, Backprop b) => Backprop (KTupleF a b Identity)
 
 splitKT ::
-     (Backprop a, Backprop b, Reifies s W)
-  => BVar s (KTuple a b)
-  -> (BVar s a, BVar s b)
+  (Backprop a, Backprop b, Reifies s W) =>
+  BVar s (KTuple a b) ->
+  (BVar s a, BVar s b)
 splitKT (splitBV -> KT a b) = (a, b)
-
--- numhask classes
-unaryOp ::
-     (HKD f1 a1 -> HKD f2 a2)
-  -> (HKD f1 b1 -> HKD f2 b2)
-  -> KTupleF a1 b1 f1
-  -> KTupleF a2 b2 f2
-unaryOp opA opB (KT a b) = KT (opA a) (opB b)
-
-binOp ::
-     (HKD f1 a1 -> HKD f2 a2 -> HKD f3 a3)
-  -> (HKD f1 b1 -> HKD f2 b2 -> HKD f3 b3)
-  -> KTupleF a1 b1 f1
-  -> KTupleF a2 b2 f2
-  -> KTupleF a3 b3 f3
-binOp opA opB (KT a b) (KT a' b') = KT (opA a a') (opB b b')
-
-instance (Additive a, Additive b) => Additive (KTuple a b) where
-  (+) = binOp (+) (+)
-  zero = KT zero zero
-
-instance (Subtractive a, Subtractive b) =>
-         Subtractive (KTuple a b) where
-  negate = unaryOp negate negate
-
-instance (Multiplicative a, Multiplicative b) =>
-         Multiplicative (KTuple a b) where
-  (*) = binOp (*) (*)
-  one = KT one one
-
-instance (Divisive a, Divisive b) =>
-         Divisive (KTuple a b) where
-  recip = unaryOp recip recip
-
-instance (Distributive a, Distributive b) => Distributive (KTuple a b)
-
-instance (StarSemiring a, StarSemiring b) => StarSemiring (KTuple a b) where
-  star = unaryOp star star
-  plus = unaryOp plus plus
-
-instance (InvolutiveRing a, InvolutiveRing b) =>
-         InvolutiveRing (KTuple a b) where
-  adj = unaryOp adj adj
-
-instance (IntegralDomain a, IntegralDomain b) => IntegralDomain (KTuple a b)
-
-instance (Field a, Field b) => Field (KTuple a b)
-
-instance (ExpField a, ExpField b) => ExpField (KTuple a b) where
-  exp = unaryOp exp exp
-  log = unaryOp log log
-
-instance (TrigField a, TrigField b) => TrigField (KTuple a b) where
-  pi = KT pi pi
-  sin = unaryOp sin sin
-  cos = unaryOp cos cos
-  asin = unaryOp asin asin
-  acos = unaryOp acos acos
-  atan = unaryOp atan atan
-  sinh = unaryOp sinh sinh
-  cosh = unaryOp cosh cosh
-  asinh = unaryOp asinh asinh
-  acosh = unaryOp acosh acosh
-  atanh = unaryOp atanh atanh
-
-instance (UpperBoundedField a, UpperBoundedField b) =>
-         UpperBoundedField (KTuple a b) where
-  -- isNaN = unaryOp isNaN isNaN
-
-instance (LowerBoundedField a, LowerBoundedField b) =>
-         LowerBoundedField (KTuple a b)
-
-instance ( Eq b
-         , Ord a
-         , Ord (KTuple c d)
-         , Ord (KTuple a b)
-         , QuotientField a c
-         , QuotientField b d
-         , Integral (KTuple c d)
-         , Subtractive c
-         , Subtractive d
-         ) =>
-         QuotientField (KTuple a b) (KTuple c d) where
-  properFraction (KT a b) = (KT ia ib, KT ra rb)
-    where
-      (ia, ra) = properFraction a
-      (ib, rb) = properFraction b
-
-instance (Multiplicative a, Signed a, Multiplicative b, Signed b) => Signed (KTuple a b) where
-  sign = unaryOp sign sign
-  abs = unaryOp abs abs
-
-instance (Normed a c, Normed b d) => Normed (KTuple a b) (KTuple c d) where
-  norm = unaryOp norm norm
-
-instance (FromIntegral a Integer, FromIntegral b Integer) => FromIntegral (KTuple a b) Integer where
-  fromIntegral_ r = KT (fromIntegral_ r) (fromIntegral_ r)
-
-instance (FromRatio a c, FromRatio b c) => FromRatio (KTuple a b) c where
-  fromRatio r = KT (fromRatio r) (fromRatio r)
-
-instance (FromRational a, FromRational b) => FromRational (KTuple a b) where
-  fromRational r = KT (fromRational r) (fromRational r)
-
